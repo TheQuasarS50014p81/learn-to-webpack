07484a521894a4a47d6e1baefb54147817401fb1
The Quasar <20722202+TheQuasarS50014p81@users.noreply.github.com>
2021-08-26 23:06:08 +0900

    install eslint-loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './style.css'
-import './style.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import './style.scss';
 
 ReactDOM.render(
   <div>Hello, React!</div>,
